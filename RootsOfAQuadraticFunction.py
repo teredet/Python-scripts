d213be9579cb307393ff1b66446d21d429a82492
teredet <artem.vanzytler@gmail.com>
2021-08-09 18:14:08 +0300

    Add Roots of a Quadratic Function

---
+++ RootsOfAQuadraticFunction.py
@@ -0,0 +1,29 @@
+def Roots(a, b, c):
+    discriminant = b ** 2 - 4 * a * c
+    if discriminant == 0:
+        root = -b / (2 * a)
+        return f'''
+        Discriminant = {discriminant}
+        Root = {root}
+        '''
+
+    elif discriminant > 0:
+        root1 = (-b + discriminant ** 0.5) / (2 * a)
+        root2 = (-b - discriminant ** 0.5) / (2 * a)
+        return f'''
+        Discriminant = {discriminant}
+        Root 1 = {root1}
+        Root 2 = {root2}
+        '''
+
+    elif discriminant < 0:
+        return f'''
+        Discriminant = {discriminant}
+        No real root.
+        '''
+
+
+if __name__ == "__main__":
+    a, b, c = (input("For quadratic function 'f(x) = ax^2 + bx + c' enter a, b, c (separated by ' '): ")).split()
+    a, b, c = int(a), int(b), int(c)
+    print(Roots(a, b, c))
