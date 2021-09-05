cda5377a5a9eed07895ea50d35430de75995b7ea
teredet <artem.vanzytler@gmail.com>
2021-09-05 16:43:46 +0300

    Update Calculator_without_if-else. Added the ability to use functions from outside. Added docstrings for functions.

--- Calculator_without_if-else.py
+++ Calculator_without_if-else.py
@@ -1,16 +1,23 @@
 import operator
 
-oper = input("Enter operator [+ - / * **]: ")
-number1 = int(input("Number 1: "))
-number2 = int(input("Number 2: "))
+def calc_without_if_else(num1, num2, oper):
+    '''
+    num1, num2 - int\n
+    oper - string\n
+    Implementation of a calculator without if-else.
+    '''
+    action = {
+    "+" : operator.add,
+    "-" : operator.sub,
+    "/" : operator.truediv,
+    "*" : operator.mul,
+    "**" : pow
+    }
+    
+    return action[oper](number1, number2)
 
-action = {
- "+" : operator.add,
- "-" : operator.sub,
- "/" : operator.truediv,
- "*" : operator.mul,
- "**" : pow
-}
- 
-print(action[oper](number1, number2))
-
+if __name__ == "__main__":
+    oper = input("Enter operator [+ - / * **]: ")
+    number1 = int(input("Number 1: "))
+    number2 = int(input("Number 2: "))
+    print(calc_without_if_else(number1, number2, oper))
